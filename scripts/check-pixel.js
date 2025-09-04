199ba0b8f4c1fec11a1496db4fc4d038ad7e94f5
Adityas203-adx <aditya@adsynergix.com>
2025-09-04 17:36:23 +0530

    Delete scripts directory

--- scripts/check-pixel.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const file = path.join(__dirname, '..', 'client', 'pixel.min.js');
-if (!fs.existsSync(file)) {
-  console.error('Missing client/pixel.min.js. Run npm run build to generate it.');
-  process.exit(1);
-}
